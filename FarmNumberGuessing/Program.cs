f60181375c8600fb0545c913d1ed8ee3da45a3d2
Peter Zantinge <p.zantinge@speedbooks.com>
2017-08-28 20:23:47 +0200

    Upgrade to .net Core 2.0

--- FarmNumberGuessing/Program.cs
+++ FarmNumberGuessing/Program.cs
@@ -1,25 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 
 namespace FarmNumberGuessing
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
-            host.Run();
-        }
     }
 }
